c7eda00d5aa3da6817963e55e39e1d74a7bb748c
J Bruno <jbrunorodrigues13@gmail.com>
2026-04-16 14:59:53 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/jbruno/workshop_mongo/domain/User.java
@@ -0,0 +1,59 @@
+package com.jbruno.workshop_mongo.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String id;
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public User setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
