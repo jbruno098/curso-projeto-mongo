2a1d3f6b9cad806cf8aea49be25a071fcc576e51
J Bruno <jbrunorodrigues13@gmail.com>
2026-04-16 19:05:32 -0300

    Referenciando os posts do usuário

--- src/main/java/com/jbruno/workshop_mongo/domain/User.java
+++ src/main/java/com/jbruno/workshop_mongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.jbruno.workshop_mongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection = "user")
 public class User implements Serializable {
@@ -12,6 +15,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User() {
     }
@@ -47,6 +53,10 @@ public class User implements Serializable {
     public User setEmail(String email) {
         this.email = email;
         return this;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
     }
 
     @Override
